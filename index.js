cfca1d4d8698793c18f473c76a2ffe725ae204f6
Owais Zahid <owais.zahid@gmail.com>
2017-03-18 14:24:36 +0800

    [OZ-0001] skeleton code for express http2 server
    Refer:
    https://github.com/expressjs/express/issues/2761#issuecomment-216912022
    Need to revisit setting request response prototypes statically.

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs');
+var http2 = require('http2');
+var app = express();
+
+express.request.__proto__ = http2.IncomingMessage.prototype;
+express.response.__proto__ = http2.ServerResponse.prototype;
+
+var options = {
+  key: fs.readFileSync('./node_modules/http2/example/localhost.key'),
+  cert: fs.readFileSync('./node_modules/http2/example/localhost.crt')
+};
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+http2.createServer(options, app).listen(3001);
